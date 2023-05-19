aa2c18bb3e6b1c965d82255090b13d8670b9de79
Yuliia <63468608+YuliiaOsypenko@users.noreply.github.com>
2023-05-19 15:01:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// 2
+console.log("Yuliia Osypenko");
+//3
+let first = "hello";
+let second = "world";
+alert(first + " " + second);
+first = second;
+alert(first + " " + second);
+//4
+const newObj = {
+  String: "Object",
+  Number: 1,
+  Boolean: true,
+  Undedined: undefined,
+  Null: null,
+};
+//5
+const isAdult = confirm("Are you of legal age?");
+console.log(isAdult);
+//6
+const name = "Yuliia";
+const surname = "Osypenko";
+const group = "vk";
+const year = 2001;
+const isMarried = false;
+console.log(typeof year + ":" + year);
+console.log(typeof isMarried + ":" + isMarried);
+console.log(typeof name + ":" + name);
+console.log(typeof surname + ":" + surname);
+console.log(typeof group + ":" + group);
+
+const nullValue = null;
+const undefinedValue = undefined;
+console.log(typeof nullValue);
+console.log(typeof undefinedValue);
+
+//7
+const login = prompt("Enter your login:");
+const email = prompt("Enter your email:");
+const password = prompt("Enter your password:");
+alert(`Dear ${login}, your email is ${email}, your password is ${password}.`);
